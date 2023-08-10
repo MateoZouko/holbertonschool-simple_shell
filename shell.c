6b1238046297387e02108641c1c03d809ea8a620
MateoZouko <mateozouko1@gmail.com>
2023-08-10 14:32:14 +0000

    Initialize variables in shell

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void our_shell(void);
+
+int main(char **argv, int argc, char **environment)
+{
+    char **array_tok = NULL, **array_paths = NULL;
+    size_t l = 0;
+    int n = 0, op = 0;
+    char *ln = NULL, *path = NULL;
+    (void)argv;
+    (void)argc;
+    
+}
